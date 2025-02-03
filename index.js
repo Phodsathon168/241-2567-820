cd2e2cec2375e758993f0282f5699bd186fd813e
Phodsathon168 <Phodsathon.w@ku.th>
2025-02-03 17:13:51 +0700

    docker

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+<<<<<<< HEAD
+function submiData () {
+    let firstNameDOM = document.querySelector('input[name=firstname]')
+    let lastNameDOM = document.querySelector('input[name=lastname]')
+    let ageDOM = document.querySelector('input[name=age]')
+    let genderDOM = document.querySelector('input[name=gender]:checked')
+    let interestDOM = document.querySelectorAll('input[name=interest]:checked')
+    let descriptionDOM = document.querySelector('textarea[name=description]')
+    let interest = '';
+
+    for (let i = 0; i < interestDOMs.length; i++) {
+        interest += interestDOMs[i].value
+        if (i < interestDOMs.length - 1) {
+            interest += ','
+        }
+    }
+    let userData = {
+        firstname: firstNameDOM.value,
+        lastname: lastNameDOM.value,
+        age: ageDOM.value,
+        gender: genderDOM.value,
+        descriptionDOM: descriptionDOM.value,
+        interest: interest
+    }
+
+    console.log('submitData',userData)
+}
+=======
 function submitData() {
     let firstNameDOM = document.querySelector('input[name = firstname]');
     let lastNameDOM = document.querySelector('input[name = lastname]');
@@ -26,3 +54,4 @@ function submitData() {
     
     console.log('submitData', userData);
     }
+>>>>>>> 6984ac9d995d88964cb4ff48d1c7e9d81dc4e7aa
